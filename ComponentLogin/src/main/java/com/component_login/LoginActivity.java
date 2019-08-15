dfda06fe04cab0672c51e50929392632965daa95
winter0503 <915540909@qq.com>
2019-08-15 19:41:57 +0800

    组件化架构开发之登录模块的初始化

---
+++ ComponentLogin/src/main/java/com/component_login/LoginActivity.java
@@ -0,0 +1,86 @@
+package com.component_login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String EXTRA_TARGET_CLASS = "EXTRA_TARGET_CLASS";
+    private EditText et_username;
+    private EditText et_password;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    /**
+     * 绑定View,添加事件
+     */
+    private void initView() {
+        et_username = (EditText) findViewById(R.id.et_username);
+        et_password = (EditText) findViewById(R.id.et_password);
+        btn_login = (Button) findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                submit();
+                break;
+        }
+    }
+
+    private void submit() {
+        // validate
+        String username = et_username.getText().toString().trim();
+        if (TextUtils.isEmpty(username)) {
+            Toast.makeText(this, "请输入用户名", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String password = et_password.getText().toString().trim();
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "请输入密码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO validate success, do something
+        Toast.makeText(this, "登录成功: 用户名:"+username+"; 密码:"+password, Toast.LENGTH_SHORT).show();
+
+        Intent intent=getIntent();
+        String targetClass =intent.getStringExtra(EXTRA_TARGET_CLASS);
+        if(!TextUtils.isEmpty(targetClass)){
+            //登录成功，跳转到目标页面
+            Intent intent1= new Intent(intent);
+            intent1.setClassName(this,targetClass);
+            startActivity(intent);
+        }
+        finish();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
